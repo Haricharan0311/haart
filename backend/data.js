f352ebdd84f62c5d8e4501fa5b6aae74ce750ed5
Gayathri G L <85672063+reecursion@users.noreply.github.com>
2023-04-27 07:51:17 +0530

    fetching-from-backend

---
+++ backend/data.js
@@ -0,0 +1,55 @@
+const data = {
+  products: [
+    {
+      name: 'Ivory boxes',
+      slug: 'ivory-box',
+      category: 'Ivory',
+      image: '/images/p1.jpg',
+      price: 250,
+      countInStock: 20,
+      artist: 'Sundar',
+      rating: 4.0,
+      numReviews: 10,
+      description: 'Box carved in premium quality ivory',
+    },
+    {
+      name: 'Wooden Elephant',
+      slug: 'wood-elephant', //what you see in URL (url friendly)
+      category: 'Woodwork',
+      image: '/images/p2.jpg', // 679px × 829px
+      price: 120,
+      countInStock: 10,
+      artist: 'Ram',
+      rating: 4.5,
+      numReviews: 10,
+      description:
+        'Intricate work of art, with embedded mirror work in wood, carved into the shape of an elephant.',
+    },
+    {
+      name: 'Madhubani Painted Vase',
+      slug: 'madhubani-vase',
+      category: 'madhubani',
+      image: '/images/p3.jpg',
+      price: 25,
+      countInStock: 15,
+      artist: 'Shivani',
+      rating: 4.5,
+      numReviews: 14,
+      description: 'Detailed madhubani art on a vase',
+    },
+    {
+      name: 'Bronze Elephant',
+      slug: 'bronze-elephant',
+      category: 'bronze',
+      image: '/images/p4.jpg',
+      price: 65,
+      countInStock: 5,
+      artist: 'Devi',
+      rating: 4.5,
+      numReviews: 10,
+      description: 'bronze elephant',
+    },
+  ],
+};
+
+export default data;
--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,17 +1,27 @@
-import data from '../data';
+//import data from '../data';
+import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
+import axios from 'axios';
 
 function HomeScreen() {
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await axios.get('/api/products'); //AJAX request to /api/products and return result
+      setProducts(result.data);
+    };
+    fetchData();
+  }, []);
   return (
     <div>
       <h1>Featured Products</h1>
       <div className="products">
-        {data.products.map((product) => (
+        {products.map((product) => (
           <div className="product" key={product.slug}>
             <Link to={`/product/${product.slug}`}>
               <img src={product.image} alt={product.name} />
             </Link>
-            <div class="product-info">
+            <div className="product-info">
               <Link to={`/product/${product.slug}`}>
                 <p>{product.name}</p>
               </Link>
