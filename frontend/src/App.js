22c1635ce16b0f4902d97e41077267a2d4d6939c
Gayathri G L <85672063+reecursion@users.noreply.github.com>
2023-04-27 05:47:38 +0530

    mini-project-lab

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/">haart</a>
+      </header>
+      <main>list products here</main>
+    </div>
+  );
+}
+
+export default App;
